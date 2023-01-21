97ee6a684354c7dc6582eb52e326118c3cd01e80
Alexander Batrakov <a.batrakov1@gmail.com>
2023-01-21 19:18:17 +0400

    Fix

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -8,49 +8,49 @@ public class Tests {
     int len = 25;
 
     @Test
-    @DisplayName("Title length above upper border")
+    @DisplayName("Title length above upper border Test")
     public void lenAboveBorderSuccessfulTest() {
         String testString = "Volvo released a new car with the following spec: V6 236HP. It will cost $22647 and going to be sold in New York only";
         String result = "Volvo released a new...";
         Assertions.assertEquals(result, ShorteningOfTitle.shorteningOfTitle(testString, len));
     }
     @Test
-    @DisplayName("Title length within border")
+    @DisplayName("Title length within border Test")
     public void lenWithinBorderSuccessfulTest() {
         String testString = "Volvo released a car";
         String result = "Volvo released a car";
         Assertions.assertEquals(result, ShorteningOfTitle.shorteningOfTitle(testString, len));
     }
     @Test
-    @DisplayName("Title length equal upper border")
+    @DisplayName("Title length equal upper border Test")
     public void lenEqualBorderSuccessfulTest() {
         String testString = "Volvo released new carsss";
         String result = "Volvo released new carsss";
         Assertions.assertEquals(result, ShorteningOfTitle.shorteningOfTitle(testString, len));
     }
     @Test
-    @DisplayName("Title length equal upper border and last character is space")
+    @DisplayName("Title length equal upper border and last character is space Test")
     public void lenEqualBorderLastCharSpaceSuccessfulTest() {
         String testString = "Volvo released new carss ";
         String result = "Volvo released new carss ";
         Assertions.assertEquals(result, ShorteningOfTitle.shorteningOfTitle(testString, len));
     }
     @Test
-    @DisplayName("Enter title on language which different from English")
+    @DisplayName("Enter title on language which different from English Test")
     public void anotherLanguageSuccessfulTest() {
         String testString = "Volvo выпустила новый автомобиль со следующей спецификацией: V6 236HP. Он будет стоить $22647 и будет продаваться только в Нью-Йорке.";
         String result = "Volvo выпустила новый...";
         Assertions.assertEquals(result, ShorteningOfTitle.shorteningOfTitle(testString, len));
     }
     @Test
-    @DisplayName("Empty title string")
+    @DisplayName("Empty title string Test")
     public void emptyStringUnSuccessfulTest() {
         String testString = "";
         String result = "Empty string, please enter title";
         Assertions.assertEquals(result, ShorteningOfTitle.shorteningOfTitle(testString, len));
     }
     @Test
-    @DisplayName("Title string is null")
+    @DisplayName("Title string is null Test")
     public void nullStringUnSuccessfulTest() {
         String testString = null;
         String result = "Empty string, please enter title";
